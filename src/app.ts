d7dfd741bf76e43468f7f8613c786dfa5fa1bcba
bsplabs <bsplabs@gmail.com>
2024-03-26 19:15:55 +0700

    add lib class validation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import { plainToClass } from 'class-transformer';
+// import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 // import _ from 'lodash';
 // declare var GLOBAL: string;
@@ -9,18 +10,29 @@ import { plainToClass } from 'class-transformer';
 import { Product } from './product.model';
 
 // ถ้าในกรณีที่มีการดึงข้อมูลจาก server หรือ db มันอาจจะยุ่งยากที่ต้องมา instantiate แต่ละ object เช่น
-const products = [{ title: 'A Carpet', price: 29.99 }, { title: 'A Book', price: 19.99 }];
+// const products = [{ title: 'A Carpet', price: 29.99 }, { title: 'A Book', price: 19.99 }];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+
+    console.log(newProd)
+  }
+});
 
 // const loadedProducts = products.map(prod => {
 //   return new Product(prod.title, prod.price);
 // });
 
 // การใช้ class-transformer
-const loadedProducts = plainToClass(Product, products);
+// const loadedProducts = plainToClass(Product, products);
 
-for (const prod of loadedProducts) {
-  console.log(prod.getInformation());
-}
+// for (const prod of loadedProducts) {
+//   console.log(prod.getInformation());
+// }
 
 // const p1 = new Product('A Book', 12.99);
 // console.log(p1);
